c8df5255d7dec833fa22b51801482523b3f6d9ae
Propelbc <167254863+Propelbc@users.noreply.github.com>
2026-03-25 01:06:23 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let index = 0;
+
+function move(step){
+    const slides = document.getElementById("slides");
+    const total = slides.children.length;
+
+    index += step;
+
+    if(index < 0) index = total-1;
+    if(index >= total) index = 0;
+
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+// Movimiento automático
+setInterval(()=> move(1), 4000);
+
+// ===== POPUP =====
+window.onload = function(){
+    if(!localStorage.getItem("popupMostrado")){
+        document.getElementById("popup").style.display = "flex";
+        localStorage.setItem("popupMostrado", "true");
+    }
+}
+
+function cerrarPopup(){
+    document.getElementById("popup").style.display = "none";
+}
